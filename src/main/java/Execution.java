15c0f703144e73f7813c911b70c7323ef533aa9e
Maciej Barć <xgqt@gentoo.org>
2022-01-06 03:11:21 +0100

    src/main/java: add Execution.java & Job.java
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ src/main/java/Execution.java
@@ -0,0 +1,8 @@
+public class Execution
+{
+    public Integer id;
+    public Integer job_id;
+    public String  date;
+    public Integer exit_code;
+    public String  exit_output;
+}
---
+++ src/main/java/Job.java
@@ -0,0 +1,9 @@
+public class Job
+{
+    public Integer id;
+    public String  command;
+    public Integer month;
+    public Integer day;
+    public Integer hour;
+    public Integer minute;
+}
