fb2f4e709a6a9141fc137b59845ce97977785206
Maciej Barć <xgqt@gentoo.org>
2022-01-07 07:49:04 +0100

    src/main/java/ExecutionService.java: add getAll method
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/main/java/ExecutionService.java
+++ src/main/java/ExecutionService.java
@@ -1,5 +1,8 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class ExecutionService
@@ -25,4 +28,33 @@ public class ExecutionService
             DBConn.closeConn(conn);
         }
     }
+
+    public static List<Execution> getAll()
+    {
+        List<Execution> result = new LinkedList<>();
+        Connection conn = DBConn.establishConn();
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM executions");
+
+            while ( rs.next() ) {
+                Integer id           = rs.getInt("id");
+                Integer job_id       = rs.getInt("job_id");
+                String  date         = rs.getString("date");
+                Integer exit_code    = rs.getInt("exit_code");
+                String  exit_output  = rs.getString("exit_output");
+                result.add(new Execution(id, job_id, date, exit_code, exit_output));
+            }
+        }
+        catch ( Exception e ) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            DBConn.closeConn(conn);
+        }
+
+        return result;
+    }
+
 }
