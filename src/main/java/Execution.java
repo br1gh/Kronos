635ff915e0ed236248aaeaf2f9122957b459952c
Maciej Barć <xgqt@gentoo.org>
2022-01-07 03:38:00 +0100

    src/main/java/Execution.java: add Execution constructor
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/main/java/Execution.java
+++ src/main/java/Execution.java
@@ -5,4 +5,14 @@ public class Execution
     public String  date;
     public Integer exit_code;
     public String  exit_output;
+
+    Execution (Integer id, Integer job_id, String date,
+         Integer exit_code, String exit_output)
+    {
+        this.id           = id;
+        this.job_id       = job_id;
+        this.date         = date;
+        this.exit_code    = exit_code;
+        this.exit_output  = exit_output;
+    }
 }
