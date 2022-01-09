0dc29360fa36e606f15f4809866e6b82e2665c98
Maciej Barć <xgqt@gentoo.org>
2022-01-09 17:16:16 +0100

    src/main/java/MainFrame.java: new stuff
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -1,19 +1,75 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import javax.swing.*;
 
 
-public class MainFrame {
+public class MainFrame
+{
+    private static String [] make_list(int start, int stop)
+    {
+        return IntStream.range(start, stop + 1)
+                .mapToObj(String::valueOf).toArray(String[]::new);
+    }
 
     public static void show()
     {
-        JFrame frame = new JFrame("Kronos");
-        frame.setSize(500, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame main_frame = new JFrame("Kronos");
+        main_frame.setSize(500, 500);
+        main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton button = new JButton("Button");
-        frame.getContentPane().add(button);
-        button.addActionListener(new ButtonPress());
+        JTabbedPane main_tabbed_pane = new JTabbedPane();
+        main_frame.getContentPane().add(main_tabbed_pane);
 
-        frame.setVisible(true);
+
+        // Add a Job
+
+        JPanel add_panel = new JPanel();
+        add_panel.setLayout(new GridLayout(6, 2));
+        main_tabbed_pane.add("Add", add_panel);
+
+        add_panel.add(new JLabel("Command: "));
+        JTextField command_panel_field = new JTextField("echo works");
+        add_panel.add(command_panel_field);
+
+        add_panel.add(new JLabel("Month: "));
+        JComboBox month_panel_field = new JComboBox(make_list(1, 12));
+        add_panel.add(month_panel_field);
+
+        add_panel.add(new JLabel("Month Day: "));
+        JComboBox m_day_panel_field = new JComboBox(make_list(1, 31));
+        add_panel.add(m_day_panel_field);
+
+        add_panel.add(new JLabel("Week Day: "));
+        JComboBox w_day_panel_field = new JComboBox(make_list(1, 7));
+        add_panel.add(w_day_panel_field);
+
+        add_panel.add(new JLabel("Hour: "));
+        JComboBox hour_panel_field = new JComboBox(make_list(0, 23));
+        add_panel.add(hour_panel_field);
+
+        add_panel.add(new JLabel("Minute: "));
+        JComboBox minute_panel_field = new JComboBox(make_list(0, 59));
+        add_panel.add(minute_panel_field);
+
+
+        // All Jobs
+
+        JPanel all_panel = new JPanel();
+        main_tabbed_pane.add("All", all_panel);
+
+
+        // Executed jobs
+
+        JPanel executed_panel = new JPanel();
+        main_tabbed_pane.add("Executed", executed_panel);
+
+
+        // JButton button = new JButton("Button");
+        // add_panel.add(button);
+        // button.addActionListener(new ButtonPress());
+
+        main_frame.setVisible(true);
     }
 
 }
