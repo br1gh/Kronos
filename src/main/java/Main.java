d579723401a1cef6ec7805fe5fa5fd62b2f4d810
Maciej Barć <xgqt@gentoo.org>
2022-01-11 05:06:33 +0100

    src/main/java/Main.java: add executor_thread
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,22 @@ public class Main
         ConfPath.ensureConfPath();
         DBConn.ensureDB();
 
+        Thread executor_thread = new Thread(() -> {
+            while ( true ) {
+                Executor.tryExecAll(JobService.getAll());
+
+                System.out.println("Sleeping until next tick ...");
+                try {
+                    Thread.sleep(60000);
+                }
+                catch ( Exception e ) {
+                    System.out.println(e.getMessage());
+                    System.exit(1);
+                }
+            }
+        });
+        executor_thread.start();
+
         Thread gui_thread = new Thread(MainFrame::show);
         gui_thread.start();
 
