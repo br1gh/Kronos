9156ffb23b2a92903a2a8a4a8cd60d4e53bbc6d6
Maciej Barć <xgqt@gentoo.org>
2022-01-16 10:24:39 +0100

    src/main/java: add license info
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of kronos.
+ *
+ * kronos is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * kronos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kronos.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Licensed under the GNU GPL v3 License
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+
 import java.awt.*;
 import java.util.stream.IntStream;
 import javax.swing.*;
